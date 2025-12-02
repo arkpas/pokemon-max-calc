ecad92ff54bcb9fda3f3a6eb4158e02981037908
apaszenda <aro2457@gmail.com>
2025-12-02 23:40:43 +0100

    feat: my pokemon have now separate card

--- src/app/components/shared/cards/pokemon-card/pokemon-card.component.ts
+++ src/app/components/shared/cards/pokemon-card/pokemon-card.component.ts
@@ -1,10 +1,9 @@
-import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { getTypeColor, getTypeBadgeClass, getDamagePercentageColor } from '../utils';
 import { CpmToLevelPipe } from '../../../../pipes/cpmToLevel.pipe';
 import { IvPercentagePipe } from '../../../../pipes/ivPercentage.pipe';
 import { Candidate } from '../../../../types/types';
-import { MyPokemonService } from '../../../../services/my-pokemon-service/my-pokemon.service';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 export enum PokemonCardTypeEnum {
@@ -20,25 +19,14 @@ export enum PokemonCardTypeEnum {
   imports: [CommonModule, CpmToLevelPipe, IvPercentagePipe, MatTooltipModule],
 })
 export class PokemonCardComponent {
-  // TODO: może trzeba opakować te inputy w jakiś obiekt?
   @Input() candidate!: Candidate;
   @Input() pokemonCardType!: PokemonCardTypeEnum;
-  @Input() isMyPokemonActionsEnabled!: boolean;
-
-  @Output() myPokemonRemovedEvent = new EventEmitter<boolean>();
 
   showDetails = false;
   readonly PokemonCardTypeEnum = PokemonCardTypeEnum;
 
-  private myPokemonService = inject(MyPokemonService);
-
   toggleDetails() {
     this.showDetails = !this.showDetails;
-  }
-
-  removeMyPokemon() {
-    this.myPokemonService.removeMyPokemon(this.candidate.myPokemonId);
-    this.myPokemonRemovedEvent.emit(true);
   }
 
   getTypeColor = getTypeColor;
